1a3c01cdc837ee3e6b48f3d88dc636c99890bcbd
Ben Boral <ben.boral@bitrise.io>
2021-07-22 09:42:20 -0500

    only starts 4core

--- start_builds.sh
+++ start_builds.sh
@@ -6,7 +6,7 @@ set -x
 
 echo "Starting builds" 
 
-workflow_names=("gen2-4_core-xcode13.0" "gen2-4_core-xcode12.5" "gen2-4_core-xcode12.4" "gen2-4_core-xcode12.3" "gen2-4_core-xcode12.2" "gen2-4_core-xcode12.1" "gen2-4_core-xcode12.0" "gen2-8_core-xcode13.0" "gen2-8_core-xcode12.5" "gen2-8_core-xcode12.4" "gen2-8_core-xcode12.3" "gen2-8_core-xcode12.2" "gen2-8_core-xcode12.1" "gen2-8_core-xcode12.0" "gen2-12_core-xcode13.0" "gen2-12_core-xcode12.5" "gen2-12_core-xcode12.4" "gen2-12_core-xcode12.3" "gen2-12_core-xcode12.2" "gen2-12_core-xcode12.1" "gen2-12_core-xcode12.0" "gen1-elite-xcode13.0" "gen1-elite-xcode12.5" "gen1-elite-xcode12.4" "gen1-elite-xcode12.3" "gen1-elite-xcode12.2" "gen1-elite-xcode12.1" "gen1-elite-xcode12.0" "gen1-standard-xcode13.0" "gen1-standard-xcode12.5" "gen1-standard-xcode12.4" "gen1-standard-xcode12.3" "gen1-standard-xcode12.2" "gen1-standard-xcode12.1" "gen1-standard-xcode12.0")
+workflow_names=("gen2-4_core-xcode13.0" "gen2-4_core-xcode12.5" "gen2-4_core-xcode12.4" "gen2-4_core-xcode12.3" "gen2-4_core-xcode12.2" "gen2-4_core-xcode12.1" "gen2-4_core-xcode12.0")
 
 for i in "${workflow_names[@]}";
 do
